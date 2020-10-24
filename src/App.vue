f05c1347e8a1f5724388aeab2b9b99444df58b8e
evopen <520dhh@gmail.com>
2020-10-24 19:48:34 +0800

    using ant design

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <a-row>
+    <input
+      type="file"
+      ref="file"
+      style="display: none"
+      v-on:change="selectRepo"
+    />
+    <a-button type="primary" shape="round" @click="$refs.file.click()">
+      {{ repo }}
+    </a-button>
+  </a-row>
+  <a-row type="flex">
+    <a-col flex="auto">
+      <a-input-search
+        v-model:value="value"
+        placeholder="input search text"
+        @search="onSearch"
+      />
+    </a-col>
+    <a-col flex="100px">
+      <a-button type="primary">
+        Unlock All
+      </a-button>
+    </a-col>
+  </a-row>
+  <a-list item-layout="horizontal"> </a-list>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      repo: "Select Repo",
+      filter: ""
+    };
+  },
+  methods: {
+    selectRepo(file: File) {
+      console.log(file.name);
+      this.repo = file.name;
+      // alert(this.repo);
+    }
   }
 });
 </script>
@@ -23,5 +59,22 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#unlock-all {
+  margin-left: 10px;
+}
+.split {
+  width: 50%;
+  z-index: 1;
+}
+
+/* Control the left side */
+.left {
+  left: 0;
+}
+
+/* Control the right side */
+.right {
+  right: 0;
 }
 </style>
